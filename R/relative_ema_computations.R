36ff875b4f6bc5ca679149dbc162530005c092d7
kapsner <lorenz.kapsner@web.de>
2025-01-11 07:07:24 +0100

    Update relative_ema_computations.R

--- R/relative_ema_computations.R
+++ R/relative_ema_computations.R
@@ -15,7 +15,7 @@ compute_relative_to_ema <- function(dataset) {
   cutoff_date <- Sys.Date() - 1 - 366/2
 
   xts_datatable <- xts_close_to_datatable(dataset)
-  ema_timepoints <- c("200", "100", "50" , "21")
+  ema_timepoints <- c("200", "100", "50" , "21", "9")
   # reduce to valid stocks
   ema_timepoints <- ema_timepoints[nrow(xts_datatable) >= as.integer(ema_timepoints)]
   xts_datatable[, (paste0("EMA_", ema_timepoints)) := lapply(
@@ -48,7 +48,7 @@ index_query_wrapper <- function(ticker_symbols) {
     data.table::rbindlist(use.names = TRUE, fill = TRUE)
 }
 
-query_data <- function(ric, src, from = Sys.Date() - 500, to = Sys.Date() - 1) {
+query_data <- function(ric, src, from_date = Sys.Date() - 500, to_date = Sys.Date()) {
   success <- FALSE
   counter <- 0
   while (!success & counter < 10) {
@@ -60,8 +60,8 @@ query_data <- function(ric, src, from = Sys.Date() - 500, to = Sys.Date() - 1) {
           Symbols = ric,
           src = src,
           auto.assign = FALSE,
-          from = Sys.Date() - 500,
-          to = Sys.Date()
+          from = from_date,
+          to = to_date
         ) |> #s, periodicity = "intraday") |>
           na.omit()
         success <- TRUE
@@ -129,7 +129,7 @@ greater_than_ema_counts <- function(index_data) {
     pattern = "rel_EMA_",
     replacement = "",
     x = all_counts_long$EMA
-  ), levels = c("21", "50", "100", "200"))
+  ), levels = c("9", "21", "50", "100", "200"))
   return(all_counts_long)
 }
 
@@ -139,12 +139,13 @@ ema_plot <- function(data, index_name) {
     mapping = ggplot2::aes(x = date, y = value, color = EMA)
   ) +
     ggplot2::geom_hline(yintercept = 90, linetype = 'dotted', col = 'red') +
+    ggplot2::geom_hline(yintercept = 50, linetype = 'dashed', col = 'gray') +
     ggplot2::geom_hline(yintercept = 10, linetype = 'dotted', col = 'red') +
     ggplot2::geom_hline(yintercept = 0, linetype = 'dashed', col = 'black') +
     ggplot2::geom_hline(yintercept = -10, linetype = 'dotted', col = 'red') +
     ggplot2::geom_line() +
     ggplot2::scale_color_manual(
-      values = c("black", "violet", "blue", "red", "orange")
+      values = c("black", "darkgreen", "blue", "pink", "red", "orange")
     ) +
     ggplot2::ylim(pmin(-10, min(data[get("EMA") == "index", get("value")])), 100) +
     ggplot2::ggtitle(index_name, subtitle = paste0(
@@ -187,10 +188,6 @@ index_data_to_long <- function(index_data, min_date, norm) {
 }
 
 combine_data <- function(all_counts_long, index_data_long) {
-  # filter index_data
-  index_data_long <-
-  # normalize index data to 50
-  #
   data_combined <- data.table::rbindlist(
     l = list(index_data_long, all_counts_long),
     use.names = TRUE
@@ -199,6 +196,6 @@ combine_data <- function(all_counts_long, index_data_long) {
     pattern = "rel_EMA_",
     replacement = "",
     x = data_combined$EMA
-  ), levels = c("index", "21", "50", "100", "200"))
+  ), levels = c("index", "9", "21", "50", "100", "200"))
   return(data_combined)
 }
